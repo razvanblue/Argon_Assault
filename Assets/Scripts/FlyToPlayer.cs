0a65b4c68606a4663d394e350fadc3bcc3c5568b
Razvan Manea <razvan.razvanblue@gmail.com>
2020-08-27 16:34:57 +0300

    Added FlyToPlayer.cs script

---
+++ Assets/Scripts/FlyToPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FlyToPlayer : MonoBehaviour {
+
+	private Transform target;
+	private float speed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target);
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
